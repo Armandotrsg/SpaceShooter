a15dd03b43c115551356c029018a2489a38fec88
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-03-05 14:36:45 -0600

    Now the bullets destroy after 5 seconds

--- Assets/Scripts/Proyectil.cs
+++ Assets/Scripts/Proyectil.cs
@@ -7,7 +7,7 @@ public class Proyectil : MonoBehaviour {
     private float speed = 2f;
     void Start()
     {
-        Destroy(gameObject, 4f); // Destroy the bullet after 4 seconds
+        Destroy(gameObject, 5f); // Destroy the bullet after 5 seconds
     }
 
     // Update is called once per frame
