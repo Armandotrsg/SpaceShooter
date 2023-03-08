9ff631cbecb9770ec92195190dff9203de7e4721
Heber Moran <hgmb15hjrmcmr@gmail.com>
2023-03-08 17:44:35 -0600

    Spawn con tecla finalizado, (sigue spawneando en el mismo lugar)

--- Assets/Scripts/Nuevos Scripts/Spawn.cs
+++ Assets/Scripts/Nuevos Scripts/Spawn.cs
@@ -7,14 +7,16 @@ public class Spawn : MonoBehaviour
     public GameObject hazard;
     public Vector3 spawnValues;
 
-    void Start()
+    void Update()
     {
-        SpawnEnemies();
+        if(Input.GetButtonDown("Fire3"))
+        {
+            SpawnEnemies();
+        }
     }
 
     void SpawnEnemies()
     {
-        //Cambiar ejes de spawn
         Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
         Instantiate(hazard, spawnPosition, Quaternion.identity);
     }
