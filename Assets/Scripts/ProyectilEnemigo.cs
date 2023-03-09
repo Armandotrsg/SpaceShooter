e015f635b9059d0f88543b4d9c79c3c5eb71a945
Hugo Alejandro Gomez Herrera <hugomez@gdal16662.svcs.itesm.mx>
2023-03-09 14:05:17 -0600

    Archivo DisparoEnemigo

---
+++ Assets/Scripts/ProyectilEnemigo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProyectilEnemigo : MonoBehaviour
+{
+    //variables del disparo
+    [SerializeField]private float speed = 2f;
+    [SerializeField] private float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5f); // Destroy the bullet after 5 seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //y = -1 porque tiene que ir para abajo
+        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * speed, Space.World); // Move the bullet
+    }
+
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     if(other.CompareTag("Player")){
+    //         other.GetComponent<Player>().Damage(damage);
+    //         Destroy(gameObject);
+    //     }
+    // }
+}
